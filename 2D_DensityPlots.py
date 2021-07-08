2f0048eb223cc5bdfedaddca84ad2a5fba684cd7
KathrynColdham <kathryn.coldham@cern.ch>
2021-07-08 14:57:52 +0200

    Array printed for the responses

--- 2D_DensityPlots.py
+++ 2D_DensityPlots.py
@@ -8,38 +8,41 @@ df = pd.read_csv("CMSInternalCommunicationsFeedbackForm.csv", index_col=0)
 #2D array for the axes
 Array_Age_Freq = [['16-25', '26-35', '36-45', '46-55', '56-65', '66-75', '75+'], ['Never heard of it', 'Never', 'Occasionally', 'Often']]
 
-print ''
-print Array_Age_Freq
-print ''
 
 #creating an empty 4x7 array
 w, h = 7, 4;
 Array = [[0 for x in range(w)] for y in range(h)] 
 
-print len(Array_Age_Freq[0])
 
 #filling the array
 for i in range(len(Array_Age_Freq[0])):
 	
 	Age_Subset = df[df['2. In which age range do you fall under?'].str.contains(Array_Age_Freq[0][i])] #looping over the ages
-	
-	Never_heard = Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[0][i]).value_counts() #never heard of it
-	Never = Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][1]).value_counts() #never 
-	Occasionally = Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][2]).value_counts() #occasionally
-	Often = Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][3]).value_counts() #often
+		
+	#never heard of it
+	Never_heard = Age_Subset[Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][0])]
+	Never_heard_count = Never_heard['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].count() 
 
-	print Never_heard
+	#never
+	Never = Age_Subset[Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][1])]
+        Never_count = Never['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].count()	
+ 
+	#occasionally
+	Occasionally = Age_Subset[Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][2])]
+        Occasionally_count = Never['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].count()
 
-	Array[0][i] = Never_heard
-	Array[1][i] = Never
-	Array[2][i] = Occasionally
-	Array[3][i] = Often
+	#often		
+	Often = Age_Subset[Age_Subset['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].str.contains(Array_Age_Freq[1][3])]
+        Often_count = Never['9. How often do you read the following webpages? [CERN Document Server (CDS)]'].count()
 
 
-#print Array_Age_Freq[1]
-#print Array_Age_Freq[0]
-#print ' ' 
-#print Array
+	Array[0][i] = Never_heard_count
+	Array[1][i] = Never_count
+	Array[2][i] = Occasionally_count
+	Array[3][i] = Often_count
+
+
+print Array
 
 #plotting the contour plot
 #plt.contour(Array_Age_Freq[1], Array_Age_Freq[0], Array)

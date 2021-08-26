67497cc2127aac67af5ed0303a9b00a794ac3c2a
KathrynColdham <kathryn.coldham@cern.ch>
2021-08-26 14:45:28 +0100

    Percentage conversion included for cms areas

--- ScatterPlots.py
+++ ScatterPlots.py
@@ -461,6 +461,9 @@ def reader(filename):
     
 		counts = new_responses.groupby([Col2, Col1]).agg({Col1:'count'})
 
+	# convert to percentage
+	new_counts = counts.groupby(level=1).apply(lambda x:100 * x / float(x.sum()))
+
 	# Make the plot
 	sc = plt.scatter(counts.index.get_level_values(1), counts.index.get_level_values(0), c=counts.values.flatten(), cmap=plt.cm.viridis, marker="s")
 
